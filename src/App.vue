581e05d2f85492e371bcbd9c10e4ee50a9f5d12b
zhaozhihan <250002174@qq.com>
2017-05-27 21:18:13 +0800

    zhao

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    数据库的航空首家爱好的卡萨还看电视ss
   </div>
 </template>
 
@@ -9,6 +8,7 @@
 import '@/static/css/reset.css'
 import '@/static/css/common.css'
 import '@/static/css/swiper.css'
+import '@/static/css/animate.css'
 import '@/static/css/animate.css'
 export default {
   name: 'app',
---
+++ src/view/Login/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login-box">
+    <div class="login-head">
+      <h1>菜鸟学堂</h1>
+      <h2>与世界分享你的知识、经验和见解</h2>
+    </div>
+    <div class="login-main">
+      <div class="login-sel">
+        <a href="javascript:;" class="login-bor">注册</a>
+        <a href="javascript:;">登录</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+
+<style lang="less" scoped>
+.login-box{
+  background-color: #f7fafc;
+  position: fixed;
+  top:0;
+  left:0;
+  right:0;
+  bottom: 0;
+  .login-head{
+    text-align: center;
+    h1,h2{
+      font-weight: 400;
+    }
+    h1{
+      font-size: .4rem;
+      padding: .2rem 0;
+      color:#0f88eb;
+    }
+    h2{
+      padding-bottom: .2rem;
+    }
+  }
+}
+</style>
