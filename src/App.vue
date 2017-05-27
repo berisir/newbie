2c413eb7fb4dad29f22355caf637f2976f2cdb07
bei <305134251@qq.com>
2017-05-27 16:53:40 +0800

    newbie项目

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import '@/static/css/reset.css'
+import '@/static/css/common.css'
+import '@/static/css/swiper.css'
+import '@/static/css/animate.css'
+export default {
+  name: 'app',
+  created (){
+  }
+}
+</script>
+
+<style lang="less">
+#app{
+
+
+
+} 
+
+</style>
